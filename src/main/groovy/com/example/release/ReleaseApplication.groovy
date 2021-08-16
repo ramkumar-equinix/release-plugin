22d6a5b764557502bedfc1186f7edc3e7fd3bfe6
Ramkumar Pillai <ramkumar@ap.equinix.com>
2021-08-16 21:32:49 +0800

    testing

---
+++ src/main/groovy/com/example/release/ReleaseApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.release
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ReleaseApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ReleaseApplication, args)
+	}
+
+}
